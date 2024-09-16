c960793a66444a0e1d6ff50ed0510b7e10f2bed3
Sindongyeob <162396050+Sindongyeob@users.noreply.github.com>
2024-09-16 13:26:29 +0900

    print_date
    Automatically enter the date according to the cell format, even if you do not enter the date according to the format
    A program that receives three integers and outputs them according to the format
    Conditions)
    year: Right-aligned with field width of 4 digits; zero spaces that are not filled if they are less than 4 digits
    month: If you have a field width of 2 digits and less than 2 digits, the empty space is 0 instead
    day: If you have a field width of 2 digits and less than 2 digits, the empty space is 0 instead
    ex) input:1 1 1 output:0001-01-01

---
+++ print_date.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+using namespace std;
+
+/*
+서식에 맞춰서 날짜를 입력하지 않아도 자동으로 셀 서식에 맞게 날짜를 입력
+세 개의 정수를 입력 받아 서식에 맞게 출력하는 프로그램
+조건)
+year:4자리의 필드 폭을 가지고 우측 정렬하여 출력, 4자리 미만이면 채워지지 않은 빈자리는 0으로 대신
+month:2자리의 필드 폭을 가지고 2자리 미만이면 빈자리는 0으로 대신
+day:2자리의 필드 폭을 가지고 2자리 미만이면 빈자리는 0으로 대신
+ex) input:1 1 1  output:0001-01-01
+*/
+
+void print_date() {
+	int year, month, day;
+	cin >> year >> month >> day;
+	if (year >= 1 || year<=9999) {
+		cout.width(4);
+		cout.fill('0');
+		cout << year;
+	}
+	if (month >= 1 || month<=21) {
+		cout << "-";
+		cout.width(2);
+		cout.fill('0');
+		cout << month;
+	}
+	if (day >= 1 || year<=31) {
+		cout << "-";
+		cout.width(2);
+		cout.fill('0');
+		cout << day<<endl;
+	}
+	else {
+		cout << "입력범위를 초과";
+		exit(0);
+	}
+}
+
+int main()
+{
+	print_date();
+	return 0;
+}
